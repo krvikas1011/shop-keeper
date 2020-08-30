74a68480c5a5d70acce5f68bf9fae02ad42014d6
Vikas <vikas.kumar@target.com>
2020-08-30 14:57:09 +0530

    Added project structure

---
+++ code/server/shop-keeper/src/main/java/com/akv/opensource/shopkeeper/ShopKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.akv.opensource.shopkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopKeeperApplication.class, args);
+	}
+
+}
